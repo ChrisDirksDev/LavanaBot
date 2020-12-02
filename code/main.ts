cb71d1d6dcb5977efc7b600595384287401572c2
Chris Dirks <chrisdirks.developer@gmail.com>
2020-12-02 07:07:35 -0400

    linting, formatting, bot login working

--- code/main.ts
+++ code/main.ts
@@ -1,1 +1,44 @@
-const {Client} = require("discord.js");
+import { Client } from "discord.js";
+
+import config from "../config.json";
+
+
+let client: Client;
+
+const initClient = () => {
+  client = new Client();
+  
+  client.on("ready", () => {
+    console.log(`Logged in as ${client.user?.username}!`);
+  });
+  
+  client.login(config.token).then(async (res: string) => {
+    if (res != undefined) {
+      await sleep(60000)();
+      initClient();
+    }
+  });
+
+}
+
+initClient();
+
+//#region Util Functions
+const sleep = (duration: number) => {
+  return function () {
+    return new Promise(function (resolve, reject) {
+      setTimeout(function () {
+        resolve(undefined);
+      }, duration);
+    });
+  };
+};
+
+const log = (data : string) => {
+  console.log(data);
+};
+
+const randomIntFromInterval = (min : number, max : number) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+//#endregion
